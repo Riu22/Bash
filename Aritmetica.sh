bad5ea9a394972fe2a7d6c2128c9c4873d217cb0
Riu22 <riutordjaume@gmial.com>
2023-04-24 08:08:33 +0200

    first commit

--- Aritmetica.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "introdueix el nombre X: " X
-read -p "introdueix el nombre Y: " Y
-echo "has introduit els nombres ${X} i ${Y}"
-SUMA=$((X+Y))
-echo "${X}+${Y}=${SUMA}"
--- producte.sh
+++ producte.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+A=${1}
+B=${2}
 echo "introduciste los numeros ${1} ${2}"
-SUMA=$((${1}*${2}))
+SUMA=$((${B}*${A}))
 echo "el resultado de la multiplicacion ${1}*${2} es ${SUMA}"
